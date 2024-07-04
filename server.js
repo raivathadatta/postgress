cc8e1d8b47ffb1b5bdfe0f2ad208b0ef7da0d047
raivatha datta <raivatha.28.2@mountblue.tech>
2024-07-04 12:52:58 +0530

    added cors ot the app

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require('express');
+const cors  = require('cors') 
 const app = express();
 const PORT = 3032
 
-const pool = require('./app/models/db.js');
+// const pool = require('./app/models/db.js');
 const { createEmployee } = require('./app/controllers/employee.controller.js');
 
 app.use(express.json());
 
+var corsOptions = {
+    origin: "http://localhost:3032"
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
@@ -15,28 +21,6 @@ app.get("/", (req, res) => {
 })
 require("./app/routes/employee.routs.js")(app)
 
-
-// app.post('/employees', async (req, res) => {
-//     //// pass the response to controller 
-//     console.log(req.body, "helloeheheh")
-//     // try {
-//     const response = await createEmployee(req.body)
-//     if (response.error) {
-//         res.status(500).json({ error: 'Internal Server Error' });
-//         return;
-//     }
-//     // const { rows } = await pool.query('SELECT * FROM employees');
-//     // const employeeObj = req.body
-
-//     // console.log(rows)
-//     console.log('res')
-//     console.log(response.data)
-//     res.json(response.data);
-//     // } catch (err) {
-//     //     console.error('Error executing query', err);
-
-//     // }
-// });
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
